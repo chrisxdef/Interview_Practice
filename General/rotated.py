c89c1a25ee8a51525f98c1b061ba56b5f268f5b3
Chris Heisler <cheisler72@gmail.com>
2018-05-11 01:44:09 +0000

    Update

---
+++ General/rotated.py
@@ -0,0 +1,15 @@
+
+def is_rotated( a, b ):
+    passed  = []
+    start   = a[0]
+    j       = 1
+    found   = False
+    for i in b:
+        if found:
+
+        else:
+            if i != start:
+                passed.append(i)
+            else:
+                found = True
+
